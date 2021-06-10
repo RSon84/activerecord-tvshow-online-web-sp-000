36df2b4db4b064bc432d498b862aa6854d67d60e
Devin Wilson <craigvin84@yahoo.com>
2021-06-10 13:12:58 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,6 +1,10 @@
 class Show < ActiveRecord::Base
 
+<<<<<<< HEAD
   def self.highest_rating
+=======
+  def highest_rating
+>>>>>>> 8248a796da3c0be69e62f0c17a4c1610e9ffbda0
     Show.maximum(:rating)
 end
 
